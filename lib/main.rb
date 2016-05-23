60fa8f27ef7fa154433289a3626d1f96c27b49a0
Faith Oladele <faitholadele@gmail.com>
2016-05-23 13:41:56 -0600

    combing

--- lib/main.rb
+++ lib/main.rb
@@ -4,7 +4,21 @@ Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file}
 class Main
 	def initialize
 		player = Mechanics::Player.new
+		puts "1. Continue"
+		puts "2. But I am a lady"
+
+		input = gets.strip
+
+		if input =='1'
+
+			puts "duel or somthing"
+
+		else
+			puts "Welcome Lady #{player.name}"
+		end
+					
 	end
+
 end
 
-# test = Main.new
+test = Main.new
--- lib/mechanics/player.rb
+++ lib/mechanics/player.rb
@@ -1,23 +1,19 @@
 module Mechanics
 	class Player
-		attr_accessor :name, :stroy
+		attr_accessor :name, :story
 
 		def initialize
 			@name = ''
-			@story = ''
+			@story = "You wake up in the woods in the afternoon, with a huge headache."
+			puts @story
 			player_name
 		end
 
 		def player_name
-			puts "Enter Name:"
+			puts "What is your name?' A loud voice yells from nearby."
 			name = gets.strip
 			@name = name
 			puts "Welcome Sir #{@name}!"
-
-			puts "1. continue"
-			puts "2 But I am a lady"
-
-			input = gets.strip
 		end
         
 
