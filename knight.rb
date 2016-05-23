32c243b9981c5847415a6ed7e1c22873e4331191
Faith Oladele <faitholadele@gmail.com>
2016-05-23 14:47:14 -0600

    player player

--- knight.rb
+++ knight.rb
@@ -1,2 +1,19 @@
 require 'pry'
 require 'sinatra'
+# require_relative 'lib/main.rb'
+# require 'lib/main.rb'
+
+Dir[File.dirname(__FILE__) + '/lib/mechanics/*.rb'].each {|file| require file}
+
+include Mechanics
+
+get '/' do
+	erb :player
+end
+
+post '/name' do
+	@name = params[:name]
+    @player = Mechanics::Player.new(@name)
+    erb :story
+end
+
--- lib/mechanics/player.rb
+++ lib/mechanics/player.rb
@@ -2,20 +2,9 @@ module Mechanics
 	class Player
 		attr_accessor :name, :story
 
-		def initialize
-			@name = ''
-			@story = "You wake up in the woods in the afternoon, with a huge headache."
-			puts @story
-			player_name
-		end
-
-		def player_name
-			puts "What is your name?' A loud voice yells from nearby."
-			name = gets.strip
+		def initialize(name)
 			@name = name
-			puts "Welcome Sir #{@name}!"
 		end
-        
 
 	end
 end
